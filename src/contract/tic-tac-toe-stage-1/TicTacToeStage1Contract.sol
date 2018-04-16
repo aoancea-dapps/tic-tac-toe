8964fda42ed915a14abcbae76e2897592687ed3f
Alex Oancea <alx.oancea@gmail.com>
2018-04-16 23:11:04 +0300

    board table version 1

--- src/contract/tic-tac-toe-stage-1/TicTacToeStage1Contract.sol
+++ src/contract/tic-tac-toe-stage-1/TicTacToeStage1Contract.sol
@@ -19,91 +19,83 @@ contract TicTacToeStage1Contract {
     struct Board {
         address win;
 
-        mapping (uint => string) state;
+        Board3x3State state;
+        
         mapping (address => string) player_value;
     }
 
-    mapping (address => address) p1_to_p2;
-    mapping (address => address) p2_to_p1;
+    mapping (address => address) player_vs_player;
 
     mapping (address => Board) gameStates;
 
-    //mapping (string => string) games;
-    //mapping (string => string) gameStates;
-
-    function SimpleAuction () public {
-    }
-
-
     function startGame(address player1, address player2) public {
-        p1_to_p2[player1] = player2;
-        p2_to_p1[player2] = player1;
+        player_vs_player[player1] = player2;
+        player_vs_player[player2] = player1;
 
-        gameStates[player1] = Board({win: address(0)});
-        gameStates[player2] = gameStates[player1];
-
-        Board storage board = gameStates[player1];
-        board.player_value[player1] = "X";
-        board.player_value[player2] = "0";
-
-
-        // player_value[player1] = "X";
-        // player_value[player2] = "0";
-
-        // var player1AsString = toString(player1);
-        // var player2AsString = toString(player2);
-
-        // var gameIndex = strConcat(player1AsString, player2AsString);
-        // games[gameIndex] = "";
-        // gameStates[gameIndex] = "aaabbbccc";
+        Board memory boardPlayer1 = Board(address(0), Board3x3State("","","", "","","","","",""));
+        gameStates[player1] = boardPlayer1;
+        gameStates[player1].player_value[player1] = "X";
+        gameStates[player1].player_value[player2] = "O";
+        
+        Board memory boardPlayer2 = Board(address(0), Board3x3State("","","", "","","","","",""));
+        gameStates[player2] = boardPlayer2;
+        gameStates[player2].player_value[player1] = "X";
+        gameStates[player2].player_value[player2] = "O";
     }
 
     function sendPick(address player, uint boxIndex) public {
-        address adversary = p2_to_p1[player];
-
-        if(adversary == address(0))
-            adversary = p1_to_p2[player];
+        address adversary = player_vs_player[player];
 
         Board storage board = gameStates[player];
-        string player_value = board.player_value[player];
+        string storage player_value = board.player_value[player];
 
-        board.state[boxIndex] = player_value;
+        Board3x3State storage state = board.state;
+
+        if (boxIndex == 0) {
+            state.box11 = player_value;
+        } else if (boxIndex == 1) {
+            state.box12 = player_value;
+        } else if (boxIndex == 2) {
+            state.box13 = player_value;
+        } else if (boxIndex == 3) {
+            state.box21 = player_value;
+        } else if (boxIndex == 4) {
+            state.box22 = player_value;
+        } else if (boxIndex == 5) {
+            state.box23 = player_value;
+        } else if (boxIndex == 6) {
+            state.box31 = player_value;
+        } else if (boxIndex == 7) {
+            state.box32 = player_value;
+        } else if (boxIndex == 8) {
+            state.box33 = player_value;
+        }
+        
+        gameStates[player] = board;
+        gameStates[adversary] = board;
     }
-
-
-    function strConcat(string _a, string _b, string _c, string _d, string _e) internal returns (string) {
-        bytes memory _ba = bytes(_a);
-        bytes memory _bb = bytes(_b);
-        bytes memory _bc = bytes(_c);
-        bytes memory _bd = bytes(_d);
-        bytes memory _be = bytes(_e);
-        string memory abcde = new string(_ba.length + _bb.length + _bc.length + _bd.length + _be.length);
-        bytes memory babcde = bytes(abcde);
-        uint k = 0;
-        for (uint i = 0; i < _ba.length; i++) babcde[k++] = _ba[i];
-        for (i = 0; i < _bb.length; i++) babcde[k++] = _bb[i];
-        for (i = 0; i < _bc.length; i++) babcde[k++] = _bc[i];
-        for (i = 0; i < _bd.length; i++) babcde[k++] = _bd[i];
-        for (i = 0; i < _be.length; i++) babcde[k++] = _be[i];
-        return string(babcde);
+    
+    function getLine1State(address player) constant public returns (string, string, string) {
+        Board storage board = gameStates[player];
+        
+        return (board.state.box11, board.state.box12, board.state.box13);
     }
-
-    function strConcat(string _a, string _b, string _c, string _d) internal returns (string) {
-        return strConcat(_a, _b, _c, _d, "");
+    
+    function getLine2State(address player) constant public returns (string, string, string) {
+        Board storage board = gameStates[player];
+        
+        return (board.state.box21, board.state.box22, board.state.box23);
     }
-
-    function strConcat(string _a, string _b, string _c) internal returns (string) {
-        return strConcat(_a, _b, _c, "", "");
+    
+    function getLine3State(address player) constant public returns (string, string, string) {
+        Board storage board = gameStates[player];
+        
+        return (board.state.box31, board.state.box32, board.state.box33);
     }
-
-    function strConcat(string _a, string _b) internal returns (string) {
-        return strConcat(_a, _b, "", "", "");
-    }
-
-    function toString(address x) internal returns (string) {
-        bytes memory b = new bytes(20);
-        for (uint i = 0; i < 20; i++)
-            b[i] = byte(uint8(uint(x) / (2**(8*(19 - i)))));
-        return string(b);
+    
+    function getPlayerValue(address player) constant public returns(string) {
+        Board storage board = gameStates[player];
+        
+        return board.player_value[player];
     }
 }
